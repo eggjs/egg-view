6dcad5e289de7ff8b1022dbcc76115b4cfa91b5f
serializedowen <wjh199455@gmail.com>
2020-10-29 12:09:40 +0800

    fix: changed faulty test case

--- test/fixtures/apps/ts/app.ts
+++ test/fixtures/apps/ts/app.ts
@@ -1,8 +1,9 @@
-import { Application } from 'egg';
+import { Application } from "egg";
 
 export default (app: Application) => {
-  app.view.use('newEngine', {
-    render: async () => '666',
-    renderString: async () => '666',
+  app.view.use("newEngine", {
+    render: async () => null,
+    renderView: async () => "666",
+    renderString: async () => "666",
   });
-}
+};
