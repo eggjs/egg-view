2886509628b044d374027d266ddcc5b433f3c65c
serializedowen <wjh199455@gmail.com>
2020-11-05 14:48:22 +0800

    refactor(dts): make ContextView extend from ViewBase

--- index.d.ts
+++ index.d.ts
@@ -59,46 +59,11 @@ declare module "egg" {
     view: ViewManager;
   }
 
-  interface Context {
+  interface Context extends ViewBase {
     /**
      * View instance that is created every request
      */
     view: ContextView;
-
-    /**
-     * Render a file by view engine, then set to body
-     * @param {String} name - the file path based on root
-     * @param {Object} [locals] - data used by template
-     * @param {Object} [options] - view options, you can use `options.viewEngine` to specify view engine
-     * @return {Promise<String>} result - return a promise with a render result
-     */
-    render(name: string, locals?: any, options?: RenderOptions): Promise<null>;
-
-    /**
-     * Render a file by view engine and return it
-     * @param {String} name - the file path based on root
-     * @param {Object} [locals] - data used by template
-     * @param {Object} [options] - view options, you can use `options.viewEngine` to specify view engine
-     * @return {Promise<String>} result - return a promise with a render result
-     */
-    renderView(
-      name: string,
-      locals?: any,
-      options?: RenderOptions
-    ): Promise<string>;
-
-    /**
-     * Render a template string by view engine
-     * @param {String} tpl - template string
-     * @param {Object} [locals] - data used by template
-     * @param {Object} [options] - view options, you can use `options.viewEngine` to specify view engine
-     * @return {Promise<String>} result - return a promise with a render result
-     */
-    renderString(
-      name: string,
-      locals?: any,
-      options?: RenderOptions
-    ): Promise<string>;
   }
 
   interface EggAppConfig {
